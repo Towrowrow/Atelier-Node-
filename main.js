65082146f3848f93998462aed1c30f8676171b8d
Tilmant Rémi <towtowrow@gmail.com>
2019-03-20 15:05:10 +0100

    Init

---
+++ main.js
@@ -0,0 +1,15 @@
+const chalk = require('./node_modules/chalk');
+const log = console.log;
+
+
+
+
+log(chalk.red('Anakin'));
+log(chalk.blue('Zoubida'));
+log(chalk.yellow('Tic'));
+log(chalk.red('Tac'));
+log(chalk.green('Katsuki'));
+log(chalk.yellow('Fabien'));
+log(chalk.red('Jihem'));
+log(chalk.blue('Jean-Zozo'));
+log(chalk.red('SMB'));
